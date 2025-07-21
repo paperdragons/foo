07d44ae5ff7d022373125e18e164f7292f07b581
fujunchao <frank_fjc@qq.com>
2025-07-21 22:17:10 +0800

    init

--- client.py
+++ client.py
@@ -1,56 +1,64 @@
 import argparse
+import signal
 import socket
 import _thread
 import json
+import sys
 import threading
 import time
 
-targets = []
+targets = {}
 
 
-def recv(sc):
+def recv(sk):
     while True:
-        msg, addr = sc.recvfrom(2048)
+        try:
+            msg, addr = sk.recvfrom(2048)
+        except socket.error:
+            continue
         cnt = json.loads(msg.decode("utf-8"))
         if cnt["cmd"] == 2:
-            dest = tuple(cnt['msg'])
-            k = "{}:{}".format(dest[0], dest[1])
-            print("{}:{}".format("Device joined", k))
-            targets.append(dest)
-            sc.sendto('{"cmd":0}'.encode('utf-8'), dest)
-        if cnt["cmd"] == 3:
-            print("Found other devices: " + ",".join(cnt['msg'].keys()))
-            for k, v in cnt['msg'].items():
-                targets.append(tuple(v))
-                sc.sendto('{"cmd":0}'.encode('utf-8'), tuple(v))
-        if cnt["cmd"] == 4:
-            print("{}:{}".format(addr, cnt['msg']))
+            tpr = tuple(cnt["n_peer"])
+            k = "{}:{}".format(tpr[0], tpr[1])
+            print("{} -> {}".format("New peer joined", k))
+            targets[k] = tpr
+            sk.sendto(json.dumps({"cmd":0, "msg": "hit"}).encode("utf-8"), targets[k])
+        elif cnt["cmd"] == 3:
+            if not cnt["peers"]:
+                continue
+            print("Found other peers -> " + ",".join(cnt["peers"].keys()))
+            for k, tpr in cnt['peers'].items():
+                targets[k] = tuple(tpr)
+                sk.sendto(json.dumps({"cmd":0, "msg": "hit"}).encode("utf-8"), targets[k])
+        elif cnt["cmd"] == 4:
+            print("from {}:{} -> {}".format(addr[0], addr[1], cnt['msg']))
 
-def send(sc):
+def send(sk):
     while True:
-        for x in targets:
-            sc.sendto(json.dumps({"cmd":4, "msg": "ping"}).encode("utf-8"), x)
-        time.sleep(500)
+        for tpr in targets.values():
+            sk.sendto(json.dumps({"cmd":4, "msg": "ping"}).encode("utf-8"), tpr)
+        time.sleep(5)
 
 def run(port, s_host, s_port):
     peer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     peer.bind(("0.0.0.0", port))
+    peer.settimeout(3)
+    peer.sendto(json.dumps({"cmd": 1, "msg": "registry"}).encode("utf-8"), (s_host, s_port))
 
-    msg = {"cmd":1, "msg": "registry"}
-    peer.sendto(json.dumps(msg).encode("utf-8"), (s_host, s_port))
-
-    _thread.start_new_thread(send, (peer, ))
+    threading.Thread(target=send, args=(peer,)).start()
 
     recv(peer)
 
-
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, lambda sig, frame: sys.exit(0))
+    signal.signal(signal.SIGILL, lambda sig, frame: sys.exit(0))
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", type=int, default='15100', help="the peer port")
     parser.add_argument("--server", type=str, default='127.0.0.1:7711', help="the server address")
     args = parser.parse_args()
 
-    port = args.port
-    s_host = args.server.split(":")[0]
-    s_port = int(args.server.split(":")[1])
-    run(port, s_host, s_port)
+    arg1 = args.port
+    arg2 = args.server.split(":")[0]
+    arg3 = int(args.server.split(":")[1])
+    run(arg1, arg2, arg3)
--- server.py
+++ server.py
@@ -1,33 +1,43 @@
 import argparse
 import json
+import os
+import signal
 import socket
+import sys
+import threading
 
-addrs = {}
+peers = {}
 
-def recv(sc):
+def recv(sk):
     while True:
-        msg, addr = sc.recvfrom(2048)
-        a_key = "{}:{}".format(addr[0], addr[1])
+        try:
+            msg, addr = sk.recvfrom(2048)
+        except socket.error:
+            continue
+        p_key = "{}:{}".format(addr[0], addr[1])
         cnt = json.loads(msg.decode("utf-8"))
         if cnt["cmd"] == 1:
-            if addrs:
-                for k, v in addrs.items():
-                    if k != a_key:
-                        jtt = {"cmd": 2, "msg": addr}
-                        sc.sendto(json.dumps(jtt).encode("utf-8"), v)
-                msg = {"cmd": 3, "msg": {k: v for k, v in addrs.items() if k != a_key}}
-                sc.sendto(json.dumps(msg).encode('utf-8'), addr)
-            addrs[a_key] = addr
+            if peers:
+                for k, peer in peers.items():
+                    if k != p_key:
+                        sk.sendto(json.dumps({"cmd": 2, "n_peer": addr}).encode("utf-8"), peer)
+                msg = {"cmd": 3, "peers": {k: v for k, v in peers.items() if k != p_key}}
+                sk.sendto(json.dumps(msg).encode('utf-8'), addr)
+            peers[p_key] = addr
 
 def run(port):
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind(("0.0.0.0", port))
+    server.settimeout(5)
     print("Server listening on port", port)
     recv(server)
+    server.close()
 
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, lambda sig, frame: sys.exit(0))
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", type=int, default=7711, help="Server port")
     args = parser.parse_args()
